893b38eea0d1425574f43ff467dc531bcd8025d8
vic1707 <28602203+vic1707@users.noreply.github.com>
2024-12-05 17:18:04 +0100

    base macro code

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+#![expect(
+    clippy::needless_pass_by_value,
+    clippy::todo,
+    unused_variables,
+    reason = "WIP"
+)]
+
+#[proc_macro_attribute]
+pub fn nnn(
+    nnn_args: proc_macro::TokenStream,
+    type_definition: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    expand(nnn_args, type_definition)
+        .unwrap_or_else(|err| err.to_compile_error())
+        .into()
+}
+
+fn expand(
+    nnn_args: proc_macro::TokenStream,
+    type_definition: proc_macro::TokenStream,
+) -> syn::Result<proc_macro2::TokenStream> {
+    todo!();
+}
