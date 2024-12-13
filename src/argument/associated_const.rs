c910ca02835f1378c136946fffde0cb1243bf9ea
vic1707 <28602203+vic1707@users.noreply.github.com>
2024-12-13 23:35:15 +0100

    impl Gen trait for AssociatedConst

--- src/argument/associated_const.rs
+++ src/argument/associated_const.rs
@@ -1,5 +1,8 @@
 /* Dependencies */
+use quote::{format_ident, quote};
 use syn::parse::{Parse, ParseStream};
+/* Crate imports */
+use crate::gen;
 
 #[derive(Debug)]
 pub(crate) struct AssociatedConst {
@@ -19,5 +22,38 @@ impl Parse for AssociatedConst {
             name,
             value,
         })
+    }
+}
+
+impl gen::Gen for AssociatedConst {
+    fn gen_impl(&self, _: &syn::Ident, _: &syn::Type) -> gen::Implementation {
+        let visibility = &self.visibility;
+        let const_name = &self.name;
+        let value = &self.value;
+
+        gen::Implementation::BareImpl(
+            quote! {
+                #visibility const #const_name: Self = Self(#value);
+            }
+            .into(),
+        )
+    }
+
+    fn gen_tests(&self, type_name: &syn::Ident) -> proc_macro2::TokenStream {
+        let const_name = &self.name;
+
+        let err_msg = format!(
+            "Type `{type_name}` has invalid value for associated const `{const_name}`.",
+        );
+        let test_name =
+            format_ident!("const_{const_name}_should_have_valid_value");
+
+        quote! {
+            #[test]
+            fn #test_name() {
+                let inner_value = <#type_name>::#const_name.into_inner();
+                <#type_name>::try_new(inner_value).expect(#err_msg);
+            }
+        }
     }
 }
