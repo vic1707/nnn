5ac3c756608575de11106727b1adc302fa637dd8
vic1707 <28602203+vic1707@users.noreply.github.com>
2024-12-13 19:17:23 +0100

    newtype can only be a struct containing exactly one unamed private field

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 #![expect(clippy::needless_pass_by_value, unused_variables, reason = "WIP")]
+/* Modules */
+mod utils;
+/* Crate imports */
+use utils::syn_ext::SynDataExt as _;
 /* Dependencies imports */
 use quote::quote;
-use syn::{token::Pub, Visibility};
+use syn::{token::Pub, Fields, Visibility};
 
 #[proc_macro_attribute]
 pub fn nnn(
@@ -26,6 +30,7 @@ fn expand(
     }
 
     let syn::DeriveInput {
+        ref data,
         // we make a backup for later use statement
         vis: original_visibility,
         ident: ref type_name,
@@ -33,6 +38,43 @@ fn expand(
     } = input;
     // Override visibility to public since the struct is in a private module
     input.vis = Visibility::Public(Pub::default());
+
+    let syn::Data::Struct(ref data_struct) = *data else {
+        return Err(syn::Error::new(
+            input.data.decl_span(),
+            "nnn is only supported on structs.",
+        ));
+    };
+
+    if !matches!(data_struct.fields, Fields::Unnamed(_)) {
+        return Err(syn::Error::new_spanned(
+            &data_struct.fields,
+            "nnn can only be used on structs with unnamed fields.",
+        ));
+    }
+    let mut fields = data_struct.fields.iter();
+    match (fields.next(), fields.next()) {
+        // Exactly one private field
+        (Some(field), None) if matches!(field.vis, Visibility::Inherited) => (),
+        (Some(field), None) => {
+            return Err(syn::Error::new_spanned(
+                &field.vis,
+                "nnn can only be used on structs with one unnamed private field.",
+            ));
+        },
+        (_, Some(extra_field)) => {
+            return Err(syn::Error::new_spanned(
+                extra_field,
+                "nnn can only be used on structs with exactly one field.",
+            ));
+        },
+        _ => {
+            return Err(syn::Error::new_spanned(
+                input,
+                "nnn can only be used on structs with exactly one field.",
+            ));
+        },
+    }
 
     Ok(quote! {
         #[doc(hidden)]
