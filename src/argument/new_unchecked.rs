4e968af24910422c57ea26b67a235c84f18a754d
vic1707 <28602203+vic1707@users.noreply.github.com>
2024-12-13 23:34:56 +0100

    impl Gen trait for new_unchecked

--- src/argument/new_unchecked.rs
+++ src/argument/new_unchecked.rs
@@ -1,2 +1,30 @@
+/* Crate imports */
+use crate::gen;
+/* Dependencies */
+use quote::quote;
+
 #[derive(Debug)]
 pub(crate) struct NewUnchecked;
+
+impl gen::Gen for NewUnchecked {
+    fn gen_impl(
+        &self,
+        _: &syn::Ident,
+        inner_type: &syn::Type,
+    ) -> gen::Implementation {
+        gen::Implementation::BareImpl(
+            quote! {
+                #[inline]
+                #[must_use]
+                pub const unsafe fn new_unchecked(inner: #inner_type) -> Self {
+                    Self(inner)
+                }
+            }
+            .into(),
+        )
+    }
+
+    fn gen_tests(&self, _: &syn::Ident) -> proc_macro2::TokenStream {
+        quote! {}
+    }
+}
