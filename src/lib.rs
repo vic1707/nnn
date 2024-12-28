01e01b6acf4863541faa18e31c5ba48d49c5fc6f
vic1707 <28602203+vic1707@users.noreply.github.com>
2024-12-28 14:03:05 +0100

    fix re-exports visibility

--- src/lib.rs
+++ src/lib.rs
@@ -121,6 +121,9 @@ fn expand(
 
         #[allow(clippy::pub_use, reason = "pub use can happen if struct is meant to be public.")]
         #original_visibility use #mod_name::{#type_name, #error_name};
-        #(#exports)*
+        #(
+            #[allow(clippy::pub_use, reason = "pub use can happen if struct is meant to be public.")]
+            #original_visibility #exports
+        )*
     })
 }
