f60d8f1060be1b638f4d6ec3683c8d73d827b80c
vic1707 <28602203+vic1707@users.noreply.github.com>
2025-03-10 15:16:49 +0100

    better `custom_derive_empty_generics_parameters` test

--- tests/ui/fail/custom_derive_empty_generics_parameters.rs
+++ tests/ui/fail/custom_derive_empty_generics_parameters.rs
@@ -1,6 +1,16 @@
 use nnn::nnn;
 
-#[nnn(nnn_derive(Into<>, From<>, TryFrom<>, Borrow<>, AsRef<>, Deref<>))]
-struct Float(f64);
+#[nnn(nnn_derive(Into<>))]
+struct WrapperInto(i8);
+#[nnn(nnn_derive(From<>))]
+struct WrapperFrom(i8);
+#[nnn(nnn_derive(TryFrom<>))]
+struct WrapperTryFrom(i8);
+#[nnn(nnn_derive(Borrow<>))]
+struct WrapperBorrow(i8);
+#[nnn(nnn_derive(AsRef<>))]
+struct WrapperAsRef(i8);
+#[nnn(nnn_derive(Deref<>))]
+struct WrapperDeref(i8);
 
 fn main() {}
